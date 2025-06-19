41d19fe24a5ad9c633fa655532db522891b5aa68
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-06-19 17:16:53 -0400

    added path in urls.piy in todo_main and created a urls.py in todoApp

---
+++ todoApp/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+urlpatterns = [
+    path('addTask/', views.addTask, name='addTask'), #addTask/ -> path, views.addTask - > views is file for business logic and addTask is a function in that file, addTask -> helps for routing or navigating
+]
