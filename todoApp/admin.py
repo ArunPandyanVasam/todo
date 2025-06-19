63cd5d1fc972f69c7457d1ca762bce9fbb7b3b89
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-06-19 16:22:03 -0400

    list filter and search field added in admin.py

--- todoApp/admin.py
+++ todoApp/admin.py
@@ -7,6 +7,8 @@ from .models import Task
 
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('task', 'is_completed', 'created_at', 'updated_at')
+    list_filter = ('is_completed',)
+    search_fields = ('task',)
 
 admin.site.register(Task, TaskAdmin)
 
