e592ce98554f61c6540840f3aa206dc65a9a919f
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-06-19 12:38:11 -0400

    displaying the data in frontend of uncompleted section

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from todoApp.models import Task
 
 
 def home(request):
-    return render(request, 'home.html')
+    tasks = Task.objects.filter(is_completed=False)
+    #print(tasks)
+    context = {
+        'tasks': tasks # orange task123 can also we can name it
+    }
+    return render(request, 'home.html', context)
