00c2ef12626a93dd47b3ffc376aef2c793b792b0
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-06-20 00:06:39 -0400

    filtering the completed tasks

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,9 +3,11 @@ from todoApp.models import Task
 
 
 def home(request):
-    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at') #tasks = Task.objects.filter(is_completed=False)
-    #print(tasks)
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at') # tasks = Task.objects.filter(is_completed=False),     #print(tasks)
+    completed_tasks = Task.objects.filter(is_completed=True) # print(completed_tasks)
+
     context = {
-        'tasks': tasks # orange task123 can also we can name it
+        'tasks': tasks, # orange task123 can also we can name it
+        'completed_tasks': completed_tasks
     }
     return render(request, 'home.html', context)
