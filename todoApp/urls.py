2072ed3549e6d534fa87ae330166ac32b9e60a58
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-06-20 23:26:33 -0400

    Able to revert the completed task as undo

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Add task
     path('addTask/', views.addTask, name='addTask'), #addTask/ -> path, views.addTask - > views is file for business logic and addTask is a function in that file, addTask -> helps for routing or navigating
-    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done') # create the path and pass the PK, then only we will be able to fetch the data from the databse using the primary key
+    
+    #Mark as done
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'), # create the path and pass the PK, then only we will be able to fetch the data from the databse using the primary key
+    
+    #Mark as undo
+    path('mark_as_undo/<int:pk>/', views.mark_as_undo, name='mark_as_undo'),
+
+    #Edite Feature
+
 ]
--- todoApp/views.py
+++ todoApp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Task #from todoApp.models import Task
 
 # Create your views here.
@@ -9,4 +9,13 @@ def addTask(request): #function always takes a request
     return redirect('home')
 
 def mark_as_done(request, pk):
-    return HttpResponse(pk)
+    task = get_object_or_404(Task, pk=pk) #left pk is actually the field name of the task model and right pk is the dynamic primary key we are passing. 
+    task.is_completed = True # task is object from top line and is_completed is field from model
+    task.save() # now save
+    return redirect('home')#return HttpResponse(task) #return HttpResponse(pk)
+
+def mark_as_undo(request, pk):
+    task = get_object_or_404(Task, pk=pk) #left pk is actually the field name of the task model and right pk is the dynamic primary key we are passing. 
+    task.is_completed = False # task is object from top line and is_completed is field from model
+    task.save() # now save
+    return redirect('home')#return HttpResponse(task) #return HttpResponse(pk)
